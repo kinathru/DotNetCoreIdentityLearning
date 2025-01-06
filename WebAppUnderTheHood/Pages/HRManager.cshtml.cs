516589a5d101dfad5a60b02aceaa50d4cbeaebaa
kinath <kinath.ru@gmail.com>
2025-01-05 20:16:49 -0600

    feat:authenticate weather forecast request

--- WebAppUnderTheHood/Pages/HRManager.cshtml.cs
+++ WebAppUnderTheHood/Pages/HRManager.cshtml.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using WebAppUnderTheHood.Authorization;
 using WebAppUnderTheHood.DTO;
+using WebAppUnderTheHood.Pages.Account;
 
 namespace WebAppUnderTheHood.Pages;
 
@@ -21,6 +25,12 @@ public class HRManager : PageModel
     public async Task OnGet()
     {
         var httpClient = _httpClientFactory.CreateClient("OurWebAPI");
+        var res = await httpClient.PostAsJsonAsync("Auth",
+            new Credential { Username = "admin", Password = "password" });
+        res.EnsureSuccessStatusCode();
+        var strJwt = await res.Content.ReadAsStringAsync();
+        var token = JsonConvert.DeserializeObject<JwtToken>(strJwt);
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken);
         WeatherForecastItems = await httpClient.GetFromJsonAsync<List<WeatherForecastDto>>("WeatherForecast") ?? [];
     }
 }
