ce656e454743767bf88a82cae332e5c85bb65d4a
kinath <kinath.ru@gmail.com>
2025-01-06 21:23:49 -0600

    feat:implemented user registration page

---
+++ WebAppRazorPages/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorPages.Pages.Account;
+
+public class Register : PageModel
+{
+    private readonly UserManager<IdentityUser> userManager;
+
+    [BindProperty]
+    public RegisterViewModel RegisterViewModel { get; set; } = new();
+
+    public Register(UserManager<IdentityUser> userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        // Validate email address (optional)
+
+        // Create the user
+        var user = new IdentityUser()
+        {
+            Email = RegisterViewModel.Email,
+            UserName = RegisterViewModel.Email
+        };
+
+        var result = await userManager.CreateAsync(user, RegisterViewModel.Password);
+        if (result.Succeeded)
+        {
+            return RedirectToPage("/Account/Login");
+        }
+
+        foreach (var err in result.Errors)
+        {
+            ModelState.AddModelError("Register", err.Description);
+        }
+
+        return Page();
+    }
+}
+
+public class RegisterViewModel
+{
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+}
