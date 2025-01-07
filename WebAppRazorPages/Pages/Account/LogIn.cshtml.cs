cd511fd0376f5e7cc0fb4eb2d8c6e2fb34077055
kinath <kinath.ru@gmail.com>
2025-01-06 21:42:45 -0600

    feat:implemented login page

---
+++ WebAppRazorPages/Pages/Account/LogIn.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorPages.Pages.Account;
+
+public class LogIn(SignInManager<IdentityUser> signInManager) : PageModel
+{
+    [BindProperty]
+    public CredentialViewModel CredentialViewModel { get; set; } = new();
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var result = await signInManager.PasswordSignInAsync(
+            CredentialViewModel.Username,
+            CredentialViewModel.Password,
+            CredentialViewModel.RememberMe,
+            false);
+
+        if (result.Succeeded)
+        {
+            return RedirectToPage("/Index");
+        }
+
+        ModelState.AddModelError("Login", result.IsLockedOut ? "You are locked out" : "Invalid login or password");
+
+        return Page();
+    }
+}
+
+public class CredentialViewModel
+{
+    [Required]
+    [Display(Name = "User Name")]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [Display(Name = "Remember me?")]
+    public bool RememberMe { get; set; }
+}
