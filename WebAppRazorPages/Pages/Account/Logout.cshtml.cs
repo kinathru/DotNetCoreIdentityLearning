77cc5b68d74a4528f7cee981c9fa299fcb338473
kinath <kinath.ru@gmail.com>
2025-01-07 21:14:30 -0600

    feat:implemented logout functionality

---
+++ WebAppRazorPages/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorPages.Pages.Account;
+
+public class Logout : PageModel
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public Logout(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToPage("/Account/Login");
+    }
+}
